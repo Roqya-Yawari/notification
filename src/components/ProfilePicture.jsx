9b54f7822619224e4cfd78968f8e61a579eab966
Roqya-Yawari <roqiayawari33@gmail.com>
2025-09-14 22:25:08 +0430

    delete extra files and install gh-pages

--- src/components/ProfilePicture.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-import image from "../assets/images/avatar-angela-gray.webp";
-export function ProfilePicture() {
-  return (
-    <>
-      <img src={image} alt="profile picture" />
-    </>
-  );
-}
